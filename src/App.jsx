6ebea7dcd074952d77c9bc7626b72b5cbbd6b980
Akshay Kudale <akshaykudale911@gmail.com>
2022-09-19 18:37:40 +0530

    Added component folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Header from './component/header/header'
+import Nav from './component/nav/nav'
+import About from './component/about/about'
+import Experience from './component/experience/experience'
+import Servicess from './component/servicess/servicess'
+import Testimonials from './component/testimonials/testimonials'
+import Contact from './component/contact/contact'
+import Footer from './component/footer/footer'
 
 const App = () => {
     return (
-        <div>Working on React</div>
+        //<div>Working on React</div>
+        <>
+        <Header/>
+        <Nav/>
+        <About/>
+        <Experience/>
+        <Servicess/>
+        <Testimonials/>
+        <Contact/>
+        <Footer/>
+        </>
     )
 }
 
---
+++ src/component/servicess/servicess.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './servicess.css'
+
+const servicess = () => {
+  return (
+    <div>servicess</div>
+  )
+}
+
+export default servicess
