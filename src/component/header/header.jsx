7de74d1e3dedd1dd6d495834dfa74c2f4c05d66e
Akshay Kudale <akshaykudale911@gmail.com>
2022-09-21 18:35:17 +0530

    Header text added

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-import './header'
+import './header.css'
 
 const header = () => {
   return (
-    <div>header</div>
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm </h5>
+        <h1>Akshay Kudale</h1>
+        <h5 className='text-light'>Full Stack Developer</h5>
+        
+      </div>
+    </header>
   )
 }
 
