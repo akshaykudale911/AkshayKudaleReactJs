febbcc0409744d85a0b1c9e465ae43c897ecc9f8
Akshay Kudale <akshaykudale911@gmail.com>
2022-09-23 23:00:30 +0530

    Pdf and button

---
+++ src/component/header/HeaderSocial.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeaderSocial = () => {
+  return (
+    <div className='header_socials'>
+        <a href='https://linkedin.com' target='_blank'></a>
+        <a href='https://github.com' target='_blank'></a>
+        <a href='https://dribble.com' target='_blank'></a>
+        {/* search react icons  */}
+    </div>
+  )
+}
+
+export default HeaderSocial
--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../Assets/me.png'
 
 const header = () => {
   return (
@@ -8,7 +10,13 @@ const header = () => {
         <h5>Hello I'm </h5>
         <h1>Akshay Kudale</h1>
         <h5 className='text-light'>Full Stack Developer</h5>
-        
+        <CTA/>
+
+        <div className='me'>
+          <img src={ME} alt='me'/>
+        </div>
+
+        <a href='#contact' className='scroll_down'>Scroll Down</a>
       </div>
     </header>
   )
