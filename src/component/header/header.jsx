8bdb4e0259885f14cf5b4eace6bf761570bb8bd5
Akshay Kudale <akshaykudale911@gmail.com>
2022-09-26 17:49:13 +0530

    Social Icons added

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../Assets/me.png'
+import HeaderSocial from './HeaderSocial'
 
 const header = () => {
   return (
@@ -11,6 +12,7 @@ const header = () => {
         <h1>Akshay Kudale</h1>
         <h5 className='text-light'>Full Stack Developer</h5>
         <CTA/>
+        <HeaderSocial/>
 
         <div className='me'>
           <img src={ME} alt='me'/>
