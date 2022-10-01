17e96d87a6debcb11f7e77c3f42906a4339133a2
Akshay Kudale <akshaykudale911@gmail.com>
2022-10-01 20:43:10 +0530

    NavStatus and UseState Hook

--- src/component/nav/nav.jsx
+++ src/component/nav/nav.jsx
@@ -5,11 +5,13 @@ import {AiOutlineUser} from 'react-icons/ai'
 import {BsJournalText} from 'react-icons/bs'
 import {RiServiceLine} from 'react-icons/ri'
 import {AiOutlineContacts} from 'react-icons/ai'
+import {useState} from 'react'
 
 const nav = () => {
+  const [activeNav, setActiveNav] = useState('#home');
   return (
     <nav>
-      <a href='#home'><AiOutlineHome/></a>
+      <a href='#home' className={activeNav === '#home' ? 'active' :''}><AiOutlineHome/></a>
       <a href='#about'><AiOutlineUser/></a>
       <a href='#experience'><BsJournalText/></a>
       <a href='#servicess'><RiServiceLine/></a>
