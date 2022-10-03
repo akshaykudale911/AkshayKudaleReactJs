e92c6cbce95e092f87190eaf38c4026ada00497a
Akshay Kudale <akshaykudale911@gmail.com>
2022-10-03 08:22:17 +0530

    NavIcon Navigation

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -7,7 +7,7 @@ import HeaderSocial from './HeaderSocial'
 const header = () => {
   return (
     <header>
-      <div className="container header_container">
+      <div id='home' className="container header_container">
         <h5>Hello I'm </h5>
         <h1>Akshay Kudale</h1>
         <h5 className='text-light'>Full Stack Developer</h5>
--- src/component/nav/nav.jsx
+++ src/component/nav/nav.jsx
@@ -7,17 +7,22 @@ import {RiServiceLine} from 'react-icons/ri'
 import {AiOutlineContacts} from 'react-icons/ai'
 import {useState} from 'react'
 
-const nav = () => {
+const Nav = () => {
   const [activeNav, setActiveNav] = useState('#home');
   return (
     <nav>
-      <a href='#home' className={activeNav === '#home' ? 'active' :''}><AiOutlineHome/></a>
-      <a href='#about'><AiOutlineUser/></a>
-      <a href='#experience'><BsJournalText/></a>
-      <a href='#servicess'><RiServiceLine/></a>
-      <a href='#contact'><AiOutlineContacts/></a>
+      <a href='#home' onClick={() => setActiveNav('#home')} 
+       className={activeNav === '#home' ? 'active' :''}><AiOutlineHome/></a>
+      <a href='#about' onClick={() => setActiveNav('#about')} 
+      className={activeNav === '#about' ? 'active' :''}><AiOutlineUser/></a>
+      <a href='#experience' onClick={() => setActiveNav('#experience')} 
+      className={activeNav === '#experience' ? 'active' :''}><BsJournalText/></a>
+      <a href='#servicess' onClick={() => setActiveNav('#servicess')} 
+      className={activeNav === '#servicess' ? 'active' :''}><RiServiceLine/></a>
+      <a href='#contact' onClick={() => setActiveNav('#contact')} 
+      className={activeNav === '#contact' ? 'active' :''}><AiOutlineContacts/></a>
     </nav>
   )
 }
 
-export default nav
+export default Nav
