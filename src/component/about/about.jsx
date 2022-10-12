ffaee171d333c86874cae114aa6fc5d96f53d7c9
Akshay Kudale <akshaykudale911@gmail.com>
2022-10-12 22:37:15 +0530

    Experience Data jsx

--- src/component/about/about.jsx
+++ src/component/about/about.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import "./about.css"
 import ME from '../../Assets/me-about.jpg'
-import {FaAward} from 'react-icons/fa'
-import {FiUsers} from 'react-icons/fi'
-import {VscFolderLibrary} from 'react-icons/vsc'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const about = () => {
   return (
@@ -14,35 +14,35 @@ const about = () => {
       <div className="container about_container">
         <div className="about_me">
           <div className="about_me_position">
-            <img src={ME} alt='AboutMe'/>
-            </div>
-            </div>
-            
-            <div className="about_content">
-        <div className="about_cards">
-          <article className='about_card'>
-            <FaAward className='about_icon'/>
-            <h5>Experience</h5>
-            <small>3 years working Experience</small>
-          </article>
-
-          <article className='about_card'>
-            <FiUsers className='about_icon'/>
-            <h5>Clients</h5>
-            <small>300+ Clients world wide</small>
-          </article>
-
-          <article className='about_card'>
-            <VscFolderLibrary className='about_icon'/>
-            <h5>Projects</h5>
-            <small>80+ Completed</small>
-          </article>
+            <img src={ME} alt='AboutMe' />
+          </div>
         </div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quae, esse libero repellat ipsum commodi, voluptates fugit, eaque quibusdam eius incidunt nam nemo ab vitae odio laboriosam omnis similique praesentium!
-        </p>
-        <a href='#contact' className='btn btn-primary'>Let's Talk</a></div>
-        </div>
+
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FaAward className='about_icon' />
+              <h5>Experience</h5>
+              <small>3 years working Experience</small>
+            </article>
+
+            <article className="about_card">
+              <FiUsers className='about_icon' />
+              <h5>Clients</h5>
+              <small>300+ Clients world wide</small>
+            </article>
+
+            <article className="about_card">
+              <VscFolderLibrary className='about_icon' />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quae, esse libero repellat ipsum commodi, voluptates fugit, eaque quibusdam eius incidunt nam nemo ab vitae odio laboriosam omnis similique praesentium!
+          </p>
+          <a href='#contact' className='btn btn-primary'>Let's Talk</a></div>
+      </div>
     </section>
   )
 }
--- src/component/experience/experience.jsx
+++ src/component/experience/experience.jsx
@@ -1,9 +1,85 @@
 import React from 'react'
 import './experience.css'
+import { BsPatchCheckFill } from 'react-icons/bs'
 
 const experience = () => {
   return (
-    <section id='experience'>experience</section>
+    <section id='experience'>
+      <h5>What skill I have</h5>
+      <h2>My experience</h2>
+      <div className="container experience_container">
+        <div className="experience_frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience_content">
+            <article className='experience_details'>
+              <BsPatchCheckFill />
+              <h4>HTML</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience_details'>
+              <BsPatchCheckFill />
+              <h4>CSS</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className='experience_details'>
+              <BsPatchCheckFill />
+              <h4>JavaScript</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience_details'>
+              <BsPatchCheckFill />
+              <h4>Bootstrap</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience_details'>
+              <BsPatchCheckFill />
+              <h4>jQuery</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience_details'>
+              <BsPatchCheckFill />
+              <h4>React</h4>
+              <small className='text-light'>Bigenner</small>
+            </article>
+          </div>
+        </div>
+        <div className="experience_backend">
+          <h3>Backend Development</h3>
+          <div className="experience_content">
+            <article className='experience_details'>
+              <BsPatchCheckFill />
+              <h4>NodeJs</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience_details'>
+              <BsPatchCheckFill />
+              <h4>MongoDb</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className='experience_details'>
+              <BsPatchCheckFill />
+              <h4>MySql</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience_details'>
+              <BsPatchCheckFill />
+              <h4>Python</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience_details'>
+              <BsPatchCheckFill />
+              <h4>Sql</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience_details'>
+              <BsPatchCheckFill />
+              <h4>backend</h4>
+              <small className='text-light'>Bigenner</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
